cb36055c2b3e035432de31486e2a5606dd388432
Jung Yunho <a01049048063@gmail.com>
2023-02-17 06:20:06 +0900

    BOJ12933 : 오리

---
+++ yunho/Week2/BOJ12933.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ12933 {
+
+    static String str;
+    static int res;
+    static boolean[] visited;
+
+    static void solution() {
+
+        ArrayList<Character> arr = new ArrayList<>();
+        char[] ch = { 'q', 'u', 'a', 'c', 'k' };
+        int idx = 0;
+        int res = 0;
+
+        for (int x = 0; x < str.length(); x++) {
+            arr.add(str.charAt(x));
+        }
+
+        while (true) {
+
+            for (int x = 0; x < arr.size(); x++) {
+                char next = arr.get(x);
+                char wanna = ch[idx];
+
+                if (next == wanna) {
+                    idx = (idx + 1) % 5;
+                    arr.remove(x);
+                    x--;
+                }
+            }
+
+            if (idx == 0)
+                res++;
+            else
+            {
+                System.out.println(-1);
+                return;
+            }
+            if(arr.isEmpty()) {
+                System.out.println(res);
+                return;
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str = br.readLine();
+
+        if(str.length()%5 !=0 || str.charAt(0)!='q') {
+            System.out.println(-1);
+            return;
+        }
+
+        solution();
+
+    }
+
+}
